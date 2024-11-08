ef994fe8e17550dead65c966b64386b6e1cc6dac
Mike Hovedskov <deengo113@gmail.com>
2024-11-08 22:10:01 +0100

    Added logo

--- japmap-frontend/src/layouts/default_layout.tsx
+++ japmap-frontend/src/layouts/default_layout.tsx
@@ -6,7 +6,9 @@ const Default_Layout = () => {
   return (
     <>
       <div className="Default">
-        <div className="Default_SidebarContainer">test</div>
+        <div className="Default_SidebarContainer">
+          <p className="Default_SidebarTitle">JAPMAP</p>
+        </div>
         <div className="Default_Content">
           <div className="Default_ContentWrapper">
             <Outlet />
